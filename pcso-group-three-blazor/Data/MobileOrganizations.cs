e3c1fab2be4da49567ae07380865193f8b0600d3
nsespiritu@yahoo.com <nsespiritu@yahoo.com>
2022-05-30 14:25:47 +0800

    include new component MobileOrganizations

---
+++ pcso-group-three-blazor/Data/MobileOrganizations.cs
@@ -0,0 +1,17 @@
+﻿namespace pcso_group_three_blazor.Data
+{
+	public class MobileOrganizations
+	{
+		public int MobileOrganizationsID { get; set; } = 0;
+
+		public string DepartmentName { get; set; } = "";
+
+		public string EmployeeName { get; set; } = "";
+
+		public string EmployeePosition { get; set; } = "";
+
+		public byte[]? EmployeePicture { get; set; } = null;
+
+		public string EmployeePictureURL { get; set; } = "";
+	}
+}
