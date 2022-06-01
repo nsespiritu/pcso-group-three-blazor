5e52c23519cfee81b1558fad74207649b0be2ffd
nsespiritu@yahoo.com <nsespiritu@yahoo.com>
2022-06-01 12:55:34 +0800

    Include add, edit, delete functionality for blazor using stream only

---
+++ pcso-group-three-blazor/Data/Officer.cs
@@ -0,0 +1,16 @@
+﻿namespace pcso_group_three_blazor.Data
+{
+	public class Officer
+	{
+		public int id { get; set; }
+		public string? name { get; set; }
+		public string? position { get; set; }
+		public string? department { get; set; }
+		public string? imageURL { get; set; }
+		public byte[]? image { get; set; } = null;
+		public string? description { get; set; }
+		public int officeId { get; set; }
+		public DateTime created { get; set; }
+
+	}
+}
