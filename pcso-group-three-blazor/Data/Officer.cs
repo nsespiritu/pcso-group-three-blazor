8f0dba290ffb521983ed54a27f30c28edc0b934a
nsespiritu@yahoo.com <nsespiritu@yahoo.com>
2022-06-02 14:35:21 +0800

    Final presentation

--- pcso-group-three-blazor/Data/Officer.cs
+++ pcso-group-three-blazor/Data/Officer.cs
@@ -2,15 +2,15 @@
 {
 	public class Officer
 	{
-		public int id { get; set; }
-		public string? name { get; set; }
-		public string? position { get; set; }
-		public string? department { get; set; }
-		public string? imageURL { get; set; }
+		public int id { get; set; } = 0;
+		public string? name { get; set; } = "";
+		public string? position { get; set; } = "";
+		public string? department { get; set; } = "";
+		public string? imageURL { get; set; } = "";
 		public byte[]? image { get; set; } = null;
-		public string? description { get; set; }
-		public int officeId { get; set; }
-		public DateTime created { get; set; }
+		public string? description { get; set; } = "";
+		public int officeId { get; set; } = 0;
+		public DateTime created { get; set; } = DateTime.Now;
 
 	}
 }
